de3d14af18f3562c9874fab082364d4143cfe1a5
Guilherme da Silva Mariano <guilherme.mariano@academico.ifpb.edu.br>
2024-10-28 18:50:45 -0300

    cria arquivo para a questao 04 fundamentos

---
+++ questoes/fundamentos-questao04.js
@@ -0,0 +1,9 @@
+function verificacao_num(n1, n2) {
+    return n1 === 30 || n2 === 30 || (n1 + n2) === 30;
+}
+
+let num1 = 10;s
+let num2 = 5;
+
+let resultado = verificacao_num(num1, num2);
+console.log(resultado);s
