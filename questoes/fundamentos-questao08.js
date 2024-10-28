dc4eea1e9507e1877d0160d7bd22802b73638cc4
Guilherme da Silva Mariano <guilherme.mariano@academico.ifpb.edu.br>
2024-10-28 19:35:42 -0300

    cria arquivo para a questao 08 fundamentos

---
+++ questoes/fundamentos-questao08.js
@@ -0,0 +1,6 @@
+function inverteFrase(frase) {
+    return frase1.split("").reverse().join("");
+}
+let frase1 = "Guilherme foi a praia";
+let fraseinvertida = inverteFrase(frase1);
+console.log(fraseinvertida);
