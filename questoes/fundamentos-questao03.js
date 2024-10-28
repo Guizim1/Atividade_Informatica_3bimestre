bfaaa3626ee29ad5d830045b8fdd53720fb0def4
Guilherme da Silva Mariano <guilherme.mariano@academico.ifpb.edu.br>
2024-10-28 18:39:38 -0300

    cria arquivo para a questao 03 fundamentos

---
+++ questoes/fundamentos-questao03.js
@@ -0,0 +1,9 @@
+
+let temperaturaCelsius = 25;
+let temperaturaFahrenheit = 77;
+
+let fahrenheit = (temperaturaCelsius * 9/5) + 32
+let celsius = (temperaturaFahrenheit - 32) * 5/9
+
+console.log(temperaturaCelsius + " °C é igual a " + fahrenheit + " °F");
+console.log(temperaturaFahrenheit + " °F é igual a " + celsius + " °C");
